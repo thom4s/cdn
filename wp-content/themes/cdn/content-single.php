65015aba337608b6ae9a7fed1b36d65781c2dc53
Thomas <thomasflorentin@yahoo.fr>
2015-03-26 09:09:55 +0100

    Add StarterTheme

---
+++ wp-content/themes/cdn/content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package cdn
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php cdn_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'cdn' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php cdn_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
