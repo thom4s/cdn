484b63b7b7f304e962ec134541490bdecf68e7b9
Thomas <thomasflorentin@yahoo.fr>
2015-04-23 17:32:51 +0200

    Add box in header

--- wp-content/themes/cdn/header-else.php
+++ wp-content/themes/cdn/header-else.php
@@ -1,3 +1,12 @@
+<?php 
+
+    // Just get the necessary metas 
+    $prefix = 'event_meta_';
+    $event_booking_id = rwmb_meta( $prefix . 'event_booking_id' );
+
+
+?>
+
   <header id="masthead" class="site-header l-4col l-first" role="banner">
 
     <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>  
@@ -6,5 +15,35 @@
       <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
     </nav><!-- #site-navigation -->
 
+    <div class="search-bar">
+      <input type="text">
+      <input type="submit" value="Rechercher">
+    </div>
+
+
+    <?php if( is_singular( 'event' ) ){ ?>
+        
+        <ul class="buttons-group">
+          <h5>Pratique</h5>
+          <li class="btn"><a href="http://www.forumsirius.fr/orion/sartrouville.phtml?spec=<?php echo $event_booking_id; ?>">Réservez pour ce spectacle</a></li>
+          <li class="btn"><a href="/saison">Retournez au calendrier</a></li>
+          <li class="btn"><a href="">Le spectacle suivant</a></li>
+        </ul>
+
+    <?php } ?>
+
+
+    <?php if( is_singular( 'page' ) ){ ?>
+
+      <div class="related-content">
+        
+      </div>
+
+    <?php } ?>
+
+
+
+
   </header><!-- .site-branding -->
+
 
