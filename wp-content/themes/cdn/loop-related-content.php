51f3b7e029c1d5179e4cba4344e880c91d14ae2f
Thomas <thomasflorentin@yahoo.fr>
2015-04-30 10:05:16 +0200

    Polish

--- wp-content/themes/cdn/loop-related-content.php
+++ wp-content/themes/cdn/loop-related-content.php
@@ -17,41 +17,46 @@
 
 
   <div class="related-content">
-    <h2>Aller plus loin</h2>
+    <?php if ( is_front_page() ) { ?>
+      
+    <?php } else { ?>
+      <h2>Aller plus loin</h2>
+    <?php }?>
+    
       <div id="grid" class="row" data-columns>
 
-  <?php
-    foreach($linkedposts as $linkedpost):
+          <?php
+            foreach($linkedposts as $linkedpost):
 
-      $linked_post_id = $linkedpost['defaults_meta_linkedpost-id'];
-      $linked_post_bg = $linkedpost['defaults_meta_bloc_bg'];
-      $linked_post_col = $linkedpost['defaults_meta_bloc_col'];
-      $linked_post_has_link = $linkedpost['defaults_meta_has_link'];
+              $linked_post_id = $linkedpost['defaults_meta_linkedpost-id'];
+              $linked_post_bg = $linkedpost['defaults_meta_bloc_bg'];
+              $linked_post_col = $linkedpost['defaults_meta_bloc_col'];
+              $linked_post_has_link = $linkedpost['defaults_meta_has_link'];
 
-      $linked_post = get_post($linked_post_id);
-      $post_title = $linked_post->post_title;
-      $post_url = $linked_post->guid;
-      $post_type = $linked_post->post_type;
+              $linked_post = get_post($linked_post_id);
+              $post_title = $linked_post->post_title;
+              $post_url = $linked_post->guid;
+              $post_type = $linked_post->post_type;
 
-      if($post_type == 'event'){
-        $post_excerpt = rwmb_meta(  $prefix_event . 'intro', array(), $linked_post_id );
-        $post_meta = rwmb_meta(  $prefix_event . 'event_date', array(), $linked_post_id );
-      }
-      if($post_type == 'post'){
-        $post_meta = get_the_terms( $linked_post_id, 'category' );
-        $post_excerpt = $linked_post->post_excerpt;
-      }  
-      if($post_type == 'page'){
-        $post_meta = get_the_terms( $linked_post_id, 'category' );
-        $post_excerpt = $linked_post->post_excerpt;
-      }  
+              if($post_type == 'event'){
+                $post_excerpt = rwmb_meta(  $prefix_event . 'intro', array(), $linked_post_id );
+                $post_meta = rwmb_meta(  $prefix_event . 'event_date', array(), $linked_post_id );
+              }
+              if($post_type == 'post'){
+                $post_meta = get_the_terms( $linked_post_id, 'category' );
+                $post_excerpt = $linked_post->post_excerpt;
+              }  
+              if($post_type == 'page'){
+                $post_meta = get_the_terms( $linked_post_id, 'category' );
+                $post_excerpt = $linked_post->post_excerpt;
+              }  
 
-      include(locate_template('bloc.php'));
+              include(locate_template('bloc.php'));
 
-    endforeach;
-    wp_reset_postdata(); ?>
+            endforeach;
+            wp_reset_postdata(); ?>
 
-    </div>
+      </div><!-- #grid -->
   </div><!-- .related-content -->
 
   <?php  } // end if $first_id
