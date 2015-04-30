c22b7fab1cc956e5060a093efa8614496f961e42
Thomas <thomasflorentin@yahoo.fr>
2015-04-30 09:39:02 +0200

    Add bloc in saison template

---
+++ wp-content/themes/cdn/bloc-event.php
@@ -0,0 +1,25 @@
+
+  <article class="grid-item related-post bloc-outer bloc-1col">
+    <a href="<?php the_permalink(); ?>">
+      
+      <div class="bloc-img">
+        <?php the_post_thumbnail( 'bloc-thumb', '' ); ?>
+      </div>
+      
+      <div class="bloc-content">
+        <div class="meta">
+          <?php if( is_array($post_meta) ){
+            foreach ($post_meta as $meta) {
+              echo $meta->name;
+            }
+          } else {
+            echo $post_meta; 
+          } ?>
+        </div>
+        
+        <h3><?php the_title(); ?></h3>
+        <p><?php echo $post_excerpt; ?></p>
+      </div>
+
+    </a>
+  </article><!-- end article-->
