00c38074ab3bf298baefe7ec015749f85e6108fa
Thomas <thomasflorentin@yahoo.fr>
2015-05-13 09:57:20 +0200

    Polish home feature

--- wp-content/themes/cdn/header-home.php
+++ wp-content/themes/cdn/header-home.php
@@ -1,35 +1,44 @@
 <?php
-    $prefix_event = 'event_meta_';
-    $prefix_default = 'defaults_meta_';
+  $prefix_event = 'event_meta_';
+  $prefix_default = 'defaults_meta_';
 
-    // Linked Posts
-    if ($featured_event_id = rwmb_meta(  $prefix_default . 'hpfeatured-id', get_the_ID() ) ):
-      $featured_event = get_post($featured_event_id);
-      $event_title = $featured_event->post_title;
-      $event_excerpt = $featured_event->post_excerpt;
-      $event_url = $featured_event->guid;
-      $event_type = $featured_event->post_type;
-      $event_dates = rwmb_meta(  $prefix_event . 'event_date', array(), $featured_event_id );
-?>
+  // Linked Posts
+  if ($featured_event_id = rwmb_meta(  $prefix_default . 'hpfeatured-id', get_the_ID() ) ):
+    $featured_event = get_post($featured_event_id);
+    $event_title = $featured_event->post_title;
+    $event_excerpt = $featured_event->post_excerpt;
+    $event_url = $featured_event->guid;
+    $event_type = $featured_event->post_type;
+    $event_dates = rwmb_meta(  $prefix_event . 'event_date', array(), $featured_event_id );
+    $authors =  rwmb_meta( $prefix_event . 'authors', array(), $featured_event_id ); ?>
 
-  <header class="site-branding-home plain">
-    <h1 class="site-title-home">
-      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-        <?php bloginfo( 'name' ); ?>
-      </a>
-    </h1>
-    
-    <div class="home-featured plain">  
-      <?php echo get_the_post_thumbnail( $featured_event_id, 'home-featured', '' ); ?>
-      <a href="<?php echo $event_url; ?>">
-        <div class="featured-title">
-          <div class="meta"><?php echo $event_dates; ?></div>
-          <h1 class='home-featured-title'><?php echo $event_title ?></h1>
-          <div class="home-featured-meta meta">Marguerite Duras / Sylvain Maurice</div>
-        </div>
-      </a>
-    </div>   
-  </header><!-- .site-branding -->
+    <header class="site-branding-home plain">
+      <h1 class="site-title-home">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+          <?php bloginfo( 'name' ); ?>
+        </a>
+      </h1>
+      
+      <div class="home-featured plain">  
+        <?php echo get_the_post_thumbnail( $featured_event_id, 'home-featured', '' ); ?>
+        <a href="<?php echo $event_url; ?>">
+          <div class="featured-infos">
+            <div class="featured-date"><?php echo $event_dates; ?></div>
+            <h1 class='featured-title'><?php echo $event_title ?></h1>
+            <div class="featured-authors">
+              <?php 
+                $numItems = count($authors);
+                $i = 0;
+                foreach ( $authors as $k => $author ) { 
+                  echo '<span class="">' . $author['name'] . '</span>';
+                  if(++$i !== $numItems) { echo ' / '; }
+                }
+              ?>
+            </div>
+          </div>
+        </a>
+      </div>   
+    </header><!-- .site-branding -->
   <?php endif; ?>
 
   <header id="masthead" class="site-header l-4col l-first m-4col m-first" role="banner">
