81e50edb4c5121b86525af2da5c1b12690c166ba
Thomas <thomasflorentin@yahoo.fr>
2015-05-15 08:41:11 +0200

    Add no results page

--- wp-content/themes/cdn/content-none.php
+++ wp-content/themes/cdn/content-none.php
@@ -9,18 +9,27 @@
 ?>
 
 <section class="no-results not-found">
-	<header class="page-header">
-		<h1 class="page-title"><?php _e( 'Nothing Found', 'cdn' ); ?></h1>
-	</header><!-- .page-header -->
 
-	<div class="page-content">
+	<header class="entry-header row content-part ">
+		<div class="entry-header-inner l-12col l-first l-1col-push">
+			<h1 class="entry-title"><?php _e( 'Désolé, aucun résultat...', 'cdn' ); ?></h1>
+				<?php if ( is_search() ) : ?>
+
+				<p><?php _e( 'Nous n\'avons trouvé aucun résultat pour votre recherche. Essayez avec un autre mot... ', 'cdn' ); ?></p>
+
+			<?php endif; ?>
+
+
+		</div><!-- .entry-header-inner -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content content-part p-is-formated m-6col m-1col-push m-first">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'cdn' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
 
 		<?php elseif ( is_search() ) : ?>
 
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'cdn' ); ?></p>
 			<?php get_search_form(); ?>
 
 		<?php else : ?>
